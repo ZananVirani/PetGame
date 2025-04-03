f1470d3c7ce6a4e6f6f40f1f5f516f3d374978ac
Jay Pravinbhai Prajapati <jprajap3@uwo.ca>
2025-04-03 05:50:53 +0000

    Added Javadoc comments for TitleText.java

--- TitleText.java
+++ TitleText.java
@@ -1,25 +1,34 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class TutorialTitle here.
+ * TitleText displays a large title text message on the screen. This class is used to show the title of a tutorial or any other section in the game
+ * environment.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * It creates a bold, large-sized black title on a transparent background.
+ * 
+ * Example Usage:
+ * new TitleText("Welcome to the Game!");
+ * 
+ * @author Group 78
  */
 public class TitleText extends Actor
 {
+    /**
+     * Creates a new TitleText object with the specified message. The message is displayed in large black font.
+     * 
+     * @param message The title text to display.
+     */
     public TitleText(String message) {
-        
         GreenfootImage img = new GreenfootImage(700, 300);
         img.setColor(Color.BLACK);
         img.setFont(new Font("Arial", false, false, 30));
         img.drawString(message, 10, 25);
         setImage(img);
     }
-    
+
     /**
-     * Act - do whatever the TutorialTitle wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Act method - currently does nothing.
+     * Can be used to add behavior to the title text if needed.
      */
     public void act()
     {
