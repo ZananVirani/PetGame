cab90127645201225afa5155beaab4cf5928ba17
Jay Pravinbhai Prajapati <jprajap3@uwo.ca>
2025-04-03 04:57:01 +0000

    Added Javadoc comments to ParentalToggle.java

--- ParentalToggle.java
+++ ParentalToggle.java
@@ -1,9 +1,14 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Parentaltoggle here.
+ * The ParentalToggle class represents a toggle button in the game that allows the player or user to enable or disable a parental restriction feature.
  * 
- * @author (your name) 
+ * The toggle displays two options visually: "Enable" and "Disable". When clicked, the toggle state switches between enabled and disabled, updating the
+ * button appearance accordingly.
+ * 
+ * This feature is useful for controlling access to certain game functions or limiting gameplay for younger users.
+ * 
+ * @author (Your name)
  * @version (a version number or a date)
  */
 public class ParentalToggle extends Actor
@@ -12,18 +17,33 @@ public class ParentalToggle extends Actor
     private static final int WIDTH = 200;
     private static final int HEIGHT = 60;
 
-    public ParentalToggle() {
+    /**
+     * Constructs a ParentalToggle button and initializes its visual appearance. The initial state is set to disabled.
+     */
+    public ParentalToggle()
+    {
         updateImage();
     }
 
-    public void act() {
-        if (Greenfoot.mouseClicked(this)) {
+    /**
+     * Act method - called whenever the 'Act' or 'Run' button is pressed.
+     * If the toggle button is clicked, the state is flipped (enabled/disabled) and the visual appearance is updated.
+     */
+    public void act()
+    {
+        if (Greenfoot.mouseClicked(this))
+        {
             enabled = !enabled;
             updateImage();
         }
     }
 
-    private void updateImage() {
+    /**
+     * Updates the visual appearance of the toggle button based on its current state. Displays two options "Enable" and "Disable", with the active option
+     * highlighted.
+     */
+    private void updateImage()
+    {
         GreenfootImage img = new GreenfootImage(WIDTH, HEIGHT);
         img.setColor(Color.WHITE);
         img.fill();
@@ -31,12 +51,12 @@ public class ParentalToggle extends Actor
 
         int circleY = HEIGHT / 2 - 10;
 
-        // ENABLE OPTION
+        // Enable option
         img.setColor(enabled ? Color.MAGENTA : Color.BLACK);
         img.fillOval(20, circleY, 20, 20);
         img.drawString("Enable", 50, circleY + 16);
 
-        // DISABLE OPTION
+        // Disable option
         img.setColor(!enabled ? Color.MAGENTA : Color.BLACK);
         img.fillOval(120, circleY, 20, 20);
         img.drawString("Disable", 150, circleY + 16);
@@ -44,7 +64,14 @@ public class ParentalToggle extends Actor
         setImage(img);
     }
 
-    public boolean isEnabled() {
+    /**
+     * Returns the current state of the toggle.
+     * 
+     * @return true if the parental toggle is enabled, false otherwise.
+     */
+    public boolean isEnabled()
+    {
         return enabled;
     }
 }
+
