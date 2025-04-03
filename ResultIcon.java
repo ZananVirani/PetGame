79950f72353cf80a81fcf35a08fcf4e6358ae5b5
Jay Pravinbhai Prajapati <jprajap3@uwo.ca>
2025-04-03 05:24:05 +0000

    Added Javadoc comments for ResultIcon.java

--- ResultIcon.java
+++ ResultIcon.java
@@ -1,50 +1,60 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
 /**
- * Write a description of class ResultIcon here.
+ * The ResultIcon class visually displays whether the player's answer was correct or incorrect. It shows a result image based on correctness and allows
+ * clearing the display.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Group 78
  */
-public class ResultIcon extends Actor
-{
+public class ResultIcon extends Actor {
     private boolean show;
     private boolean correct;
-    
+
+    /**
+     * Constructs a new ResultIcon and initializes its image.
+     */
     public ResultIcon(){
         updateImage();
     }
-    
+
+    /**
+     * Updates the icon image based on whether the result should be shown and if the answer was correct.
+     */
     private void updateImage(){
         if (show){
-            String imageFile = correct ? 
-            "correct_result.png" : "incorrect_result.png";
+            String imageFile = correct ? "correct_result.png" : "incorrect_result.png";
             GreenfootImage img = new GreenfootImage(imageFile);
             img.scale(50, 50);
             setImage(img);
-        } else{
+        } else {
             setImage((GreenfootImage) null);
         }
     }
-    
+
+    /**
+     * Sets whether the answer was correct and shows the result icon.
+     * 
+     * @param correct true if the answer was correct, false otherwise
+     */
     public void setCorrect(boolean correct){
         this.correct = correct;
         show = true;
-        
         updateImage();
     }
-    
+
+    /**
+     * Clears the result icon so nothing is displayed.
+     */
     public void clear(){
         show = false;
         updateImage();
     }
-    
+
     /**
      * Act - do whatever the ResultIcon wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
-        // Add your action code here.
+    public void act(){
+        // No action required
     }
 }
