7c4b9104eda1862aaf20970bc6a1fd5673a94e88
Jay Pravinbhai Prajapati <jprajap3@uwo.ca>
2025-04-03 04:53:24 +0000

    Added Javadoc comments to ParagraphText.java

--- ParagraphText.java
+++ ParagraphText.java
@@ -1,26 +1,33 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
 /**
- * Write a description of class ParagraphText here.
+ * The ParagraphText class is responsible for displaying a block of text (paragraph) in the Greenfoot environment. It creates a visual box containing the
+ * given message.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * This class is used to display instructions, descriptions, or other lengthy messages to the player within the game screen.
+ * 
+ * @author Group 78
  */
 public class ParagraphText extends Actor
 {
-    public ParagraphText(String message){
-        GreenfootImage img = new GreenfootImage(700,300);
+    /**
+     * Constructs a ParagraphText object with the specified message. The message is rendered in a black font on a large image background.
+     * 
+     * @param message The text content to be displayed as a paragraph.
+     */
+    public ParagraphText(String message)
+    {
+        GreenfootImage img = new GreenfootImage(700, 300);
         img.setColor(Color.BLACK);
         img.setFont(new Font("Arial", false, false, 15));
         img.drawString(message, 10, 25);
         setImage(img);
     }
+
     /**
-     * Act - do whatever the ParagraphText wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Act - currently no behavior.
+     * This method is called whenever the 'Act' or 'Run' button is pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+        // No specific action required.
     }
 }
