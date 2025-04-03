f1f41ac38c3b86f7160a1db0e5eef5d058a7b575
Jay Pravinbhai Prajapati <jprajap3@uwo.ca>
2025-04-03 03:59:41 +0000

    Added Javadoc comments for Cross.java

--- Cross.java
+++ Cross.java
@@ -1,29 +1,49 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
 /**
- * Write a description of class Cross here.
+ * The Cross class represents a cross (X) button in the user interface.  It is used to close or exit the current screen.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * When clicked, if the current screen is the MainMenu, it opens a BlackScreen. Otherwise, it pops the current screen and returns to the previous one.
+ * 
+ * This class enhances navigation by providing a standard exit button.
+ * 
+ * Example usage:
+ * addObject(new Cross(), 50, 50);
+ * 
+ * @author Group 78
  */
 public class Cross extends Actor
 {
-    public Cross(){
+    /**
+     * Constructs a Cross object and scales its image to 50x50 pixels.
+     */
+    public Cross()
+    {
         GreenfootImage image = getImage();
-        
-        image.scale(50,50);
+        image.scale(50, 50);
         setImage(image);
     }
-    
+
     /**
-     * Act - do whatever the Cross wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Act method - listens for mouse clicks on the Cross button.
+     * 
+     * If the current world is the MainMenu, it pushes a BlackScreen to the screen stack. Otherwise, it pops the current screen, effectively closing it
+     * and returning to the previous screen.
+     * 
+     * This method is called automatically by the Greenfoot environment in each act cycle.
      */
     public void act()
     {
-        if (Greenfoot.mouseClicked(this)){
-            if (getWorld() instanceof MainMenu) ScreenManager.push(new BlackScreen());
-            else ScreenManager.pop();
+        if (Greenfoot.mouseClicked(this))
+        {
+            if (getWorld() instanceof MainMenu) 
+            {
+                ScreenManager.push(new BlackScreen());
+            }
+            else 
+            {
+                ScreenManager.pop();
+            }
         }
     }
 }
